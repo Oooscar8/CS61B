24aab91876369625f04a031e76341d0487725c75
Oooscar8 <oscar8@sjtu.edu.cn>
2024-07-25 16:39:19 -0700

    ArrayDeque with resizing method passed ArrayDequeTest.

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -13,19 +13,37 @@ public class ArrayDeque<T> {
         nextLast = 5;
     }
 
-    /**
-    private void resize(int capacity) {
 
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+
+        int j = (nextFirst + 1) % items.length;
+        for (int i = 0; i < size; ++i) {
+            a[i] = items[j];
+            j = (j + 1) % items.length;
+        }
+
+        items = a;
+        nextFirst = capacity - 1;
+        nextLast = size;
     }
-     */
+
 
     public void addFirst(T item){
+        if (size == items.length){
+            resize(size * 2);
+        }
+
         items[nextFirst] = item;
         nextFirst = (nextFirst + items.length - 1) % items.length;
         size = size + 1;
     }
 
     public void addLast(T item){
+        if (size == items.length){
+            resize(size * 2);
+        }
+
         items[nextLast] = item;
         nextLast = (nextLast + 1) % items.length;
         size = size + 1;
@@ -57,6 +75,10 @@ public class ArrayDeque<T> {
     public T removeFirst(){
         if (isEmpty()) return null;
 
+        if (items.length >= 16 && size - 1 < items.length / 4) {
+            resize(items.length / 2);
+        }
+
         T FirstItem = items[(nextFirst + 1) % items.length];
         nextFirst = (nextFirst + 1) % items.length;
         size = size - 1;
@@ -65,6 +87,10 @@ public class ArrayDeque<T> {
 
     public T removeLast(){
         if (isEmpty()) return null;
+
+        if (items.length >= 16 && size - 1 < items.length / 4) {
+            resize(items.length / 2);
+        }
 
         T LastItem = items[(nextLast + items.length - 1) % items.length];
         nextLast = (nextLast + items.length - 1) % items.length;
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -101,7 +101,7 @@ public class ArrayDequeTest {
 
     @Test
     /** Add 8 elements to deque; check if order is correct. */
-    public void DequeTest() {
+    public void EightDequeTest() {
 
         ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
 
@@ -119,6 +119,24 @@ public class ArrayDequeTest {
 
         for (int i = 7; i > 4; i--) {
             assertEquals("Should have the same value", i, lld1.removeLast(), 0.0);
+        }
+    }
+
+    @Test
+    /* Add large number of elements to deque; check if order is correct. */
+    public void bigLLDequeTest() {
+
+        ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
+        for (int i = 0; i < 1000000; i++) {
+            lld1.addLast(i);
+        }
+
+        for (double i = 0; i < 500000; i++) {
+            assertEquals("Should have the same value", i, (double) lld1.removeFirst(), 0.0);
+        }
+
+        for (double i = 999999; i > 500000; i--) {
+            assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
     }
 
